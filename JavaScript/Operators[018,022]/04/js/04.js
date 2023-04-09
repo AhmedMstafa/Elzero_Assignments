d417d21ecc9141923cc0594d0e83907f05abcad0
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2023-04-09 06:07:42 +0200

    Add Operators Assignments

---
+++ JavaScript/Operators[018,022]/04/js/04.js
@@ -0,0 +1,9 @@
+let points = 10;
+
+// Write Your Code Here
+
+console.log(points + ++points.toString().length); // 13
+
+// Write Your Code Here
+
+console.log(points - points.toString().length); // 8;
