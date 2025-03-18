751c64aa6a619c02738b341147517084ef0739c5
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-03-18 17:54:20 +0200

    Add assignment Intro&TypeAnnotation[01,11][3]

---
+++ typeScript/Intro&TypeAnnotation[01,11]/03/public/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let value;
+function printInfo(valueOne, valueTwo) {
+    return `Value One Is ${valueOne}, Value Two Is ${valueTwo}`;
+}
+console.log(printInfo(10, 20));
+console.log(printInfo('10', '20'));
