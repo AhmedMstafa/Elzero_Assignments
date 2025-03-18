f6769f8deddd159da6a72a1f39cc8dcd71cd8d2b
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-03-18 23:15:55 +0200

    Add assignment Intro&TypeAnnotation[01,11][5]

---
+++ typeScript/Intro&TypeAnnotation[01,11]/05/public/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function reportErrors(username, age) {
+    return `Username: ${username} age : ${age}`;
+}
+console.log(reportErrors('Elzero', 40));
---
+++ typeScript/Intro&TypeAnnotation[01,11]/05/src/index.ts
@@ -0,0 +1,7 @@
+function reportErrors(username: string, age: number) {
+  // let rank = 'Professor';
+  return `Username: ${username} age : ${age}`;
+  // console.log('We Will Not Reach Here');
+}
+
+console.log(reportErrors('Elzero', 40));
