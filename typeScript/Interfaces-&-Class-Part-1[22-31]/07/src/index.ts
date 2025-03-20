d5173d1993072de09a0332d17cddfbb87ef16390
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2025-03-20 02:32:21 +0200

    Add assignment Interfaces-&-Class-Part-1[22-31]

---
+++ typeScript/Interfaces-&-Class-Part-1[22-31]/07/src/index.ts
@@ -0,0 +1,37 @@
+interface Play {
+  id: number;
+  title: string;
+  level: number | string;
+  logIn(): void;
+  logOut(msg: string): void;
+}
+
+// Create Your Class Here
+
+class Player implements Play {
+  id: number;
+  title: string;
+  level: number | string;
+  logIn: () => void;
+  logOut: (msg: string) => void;
+
+  constructor(id: number, title: string, level: number) {
+    this.id = id;
+    this.title = title;
+    this.level = level;
+    this.logIn = function () {
+      console.log('Logged In');
+    };
+    this.logOut = function (msg: string) {
+      console.log('Logged Out, Good By');
+    };
+  }
+}
+
+let player1 = new Player(100, 'Elzero', 95);
+
+console.log(player1.id); // 100
+console.log(player1.title); // "Elzero"
+console.log(player1.level); // 95
+player1.logIn(); // Logged In
+player1.logOut('Good Bye'); // Logged Out, Good Bye
